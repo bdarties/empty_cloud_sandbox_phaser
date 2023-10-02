ef7587e81ec734796a7750a7c44ee88d29afcacb
CodeSandbox <support@codesandbox.io>
2023-10-02 19:36:59 +0000

    Sandbox snapshot commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+import Phaser from "phaser";
+import selection from "./selection.js";
+// configuration générale du jeu
+var config = {
+  type: Phaser.AUTO,
+  width: 800, // largeur en pixels
+  height: 600, // hauteur en pixels
+  physics: {
+    // définition des parametres physiques
+    default: "arcade", // mode arcade : le plus simple : des rectangles pour gérer les collisions. Pas de pentes
+    arcade: {
+      // parametres du mode arcade
+      gravity: {
+        y: 300 // gravité verticale : acceleration ddes corps en pixels par seconde
+      },
+      debug: true // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
+    }
+  
+},
+scene: [selection]
+
+};
+
+// création et lancement du jeu
+var game = new Phaser.Game(config);
+game.scene.start("selection");
